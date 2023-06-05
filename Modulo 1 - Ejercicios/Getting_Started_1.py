ac7d911a74472ddb35c683e228b960918bc091b4
esperanzasc <esperanzasc24@gmail.com>
2023-06-05 14:30:07 -0500

    Ingresando la carpeta de ejercicios del modulo 1 - con ejercicio 1

---
+++ Modulo 1 - Ejercicios/Getting_Started_1.py
@@ -0,0 +1,6 @@
+print("Primer programa con python. Ingrese dos números para sumar...")
+
+num1 = int(input("Escriba el primer número: "))
+num2 = int(input("Escriba el segundo número: "))
+
+print("La suma de los dos números ingresados es = ", num1+num2)
